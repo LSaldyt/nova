b541ea50393bc92113d7cd4fe4b3ed2cb4be429d
LSaldyt <lucassaldyt@gmail.com>
2017-11-06 10:47:39 -0700

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This is a sketchy install script :)
+
+sudo apt-get install python-dev
+sudo apt-get install python3-dev
+sudo apt-get install portaudio19-dev
+sudo apt-get install python3-pip
+pip3 install pyaudio
+pip3 install SpeechRecognition
+
+
