529a9cb53664252f40613f43d2030148e50e3bcd
LSaldyt <lucassaldyt@gmail.com>
2017-11-15 14:17:35 -0700

    Adds scour testing

--- lib/switchboard.py
+++ lib/switchboard.py
@@ -1,4 +1,5 @@
 from .recognize import recognize
+from .speak     import speak
 
 class Switchboard:
     def __init__(self, functionDictionary):
@@ -6,12 +7,15 @@ class Switchboard:
 
     def run(self, subcommand, *args):
         print(subcommand, args)
-        self.functions[subcommand](*args)
+        try:
+            self.functions[subcommand](*args)
+        except KeyError:
+            print('The subcommand {} does not exist'.format(subcommand))
 
     def loop(self):
         while True:
             # Idealized usage, probably won't work :)
             speech = recognize()
             if speech != '':
-                subcommand, *args = speech.split(' ')
+                subcommand, *args = speech.lower().split(' ')
                 self.run(subcommand, *args)
